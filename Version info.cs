1571b774b01178b6fab5e527ae03d4ab27d79c0b
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-07 17:02:34 -0400

    Update Version info.cs

--- Version info.cs
+++ Version info.cs
@@ -3,7 +3,7 @@ namespace ScratchLab
     internal static class VersionInfo
     {
         public const string ModName = "ScratchLab";
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
         public const string Author = "Thatvrkidds";
 
         public static string FullName =>
